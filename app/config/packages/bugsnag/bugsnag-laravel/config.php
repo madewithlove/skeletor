ada5905af9f9e0646f2df4eea3ae119659c89830
Maxime Fabre <ehtnam6@gmail.com>
2014-10-24 17:24:06 +0200

    Add Bugsnag

---
+++ app/config/packages/bugsnag/bugsnag-laravel/config.php
@@ -0,0 +1,15 @@
+<?php return array(
+
+	/**
+	 * Set your Bugsnag API Key.
+	 * You can find your API Key on your Bugsnag dashboard.
+	 */
+	'api_key'               => 'YOUR-API-KEY-HERE',
+
+	/**
+	 * Set which release stages should send notifications to Bugsnag
+	 * E.g: array('development', 'production')
+	 */
+	'notify_release_stages' => null,
+
+);
