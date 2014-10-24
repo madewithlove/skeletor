233af5c4ff12a932fcb088493a2afd679bc00e0d
Maxime Fabre <ehtnam6@gmail.com>
2014-10-24 17:00:14 +0200

    Prepublish some providers

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -15,4 +15,22 @@ return array(
 
 	'debug' => true,
 
+	/*
+	|--------------------------------------------------------------------------
+	| Autoloaded Service Providers
+	|--------------------------------------------------------------------------
+	|
+	| The service providers listed here will be automatically loaded on the
+	| request to your application. Feel free to add your own services to
+	| this array to grant expanded functionality to your applications.
+	|
+	*/
+
+	'providers' => append_config(array(
+		'Barryvdh\Debugbar\ServicerProvider',
+		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+		'TwigBridge\ServiceProvider',
+		'Way\Generators\GeneratorsServicerProvider',
+	)),
+
 );
