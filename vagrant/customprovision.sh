b5d4ee1fc483efd01b6c98e0245dabfa721b2f80
Dieter Van der Stock <dietervds@gmail.com>
2014-12-16 17:41:39 +0100

    Custom provisioning with yaml config

---
+++ vagrant/customprovision.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This script will read the yaml configuration, and decide which extra things need to be installed 
+# YAML parsing is taken from: (taken from: https://gist.github.com/pkuczynski/8665367)
+
+# include parse_yaml function and parse our config file. Will be parsed with 'config_' prefix.
+. ./vagrant/parse_yaml.sh
+eval $(parse_yaml ./vagrant/skeletor.yaml "config_")
+
+# Elasticsearch
+if [[ $config_elasticsearch == true ]] 
+	then
+		echo ">>> Installing Elasticsearch"
+		sudo apt-get update && sh /vagrant/vagrant/scripts/elasticsearch.sh
+fi
