036f39953d5c7b52698b5297a30a077d3e8a2a08
Dieter Van der Stock <dietervds@gmail.com>
2014-12-16 18:05:42 +0100

    Remove unneeded and not working rvm re-sourcing line

--- vagrant/scripts/rvm.sh
+++ vagrant/scripts/rvm.sh
@@ -6,9 +6,6 @@ echo ">>> Installing RVM"
 echo ">>> Importing rvm public key"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 
-# Re-source RVM
-. /home/vagrant/.rvm/scripts/rvm
-
 # Re-source .profile if exists
 if [[ -f "/home/vagrant/.profile" ]]; then
     . /home/vagrant/.profile
