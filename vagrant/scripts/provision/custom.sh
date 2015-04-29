e2e60e8ca680667677b8c1ceeb6f7b25f57492bb
Xavier <xavierbarbosa@gmail.com>
2015-04-29 16:27:22 +0100

    Added vagrant.

---
+++ vagrant/scripts/provision/custom.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This script will read the yaml configuration, and decide which extra things need to be installed 
+# YAML parsing is taken from: (taken from: https://gist.github.com/pkuczynski/8665367)
+
+# include parse_yaml function and parse our config file. Will be parsed with 'config_' prefix.
+. /vagrant/vagrant/scripts/util/parse_yaml.sh
+eval $(parse_yaml /vagrant/vagrant/skeletor-config.yaml "config_")
+
+# Elasticsearch
+if [[ $config_elasticsearch == true ]] 
+	then
+		echo ">>> Installing Elasticsearch"
+		sudo apt-get update && sh /vagrant/vagrant/scripts/provision/elasticsearch.sh
+fi
+
+# Mailcatcher
+if [[ $config_mailcatcher == true ]] 
+	then
+		echo ">>> Installing Mailcatcher"
+		sudo apt-get update && sh /vagrant/vagrant/scripts/provision/mailcatcher.sh
+fi
