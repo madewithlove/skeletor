0d8e1036e5bcba36884b88008bad6b0aff232226
Tony Messias <tonyzrp@gmail.com>
2017-02-01 21:07:53 -0300

    Move docs to resources folder

--- dredd.js
+++ dredd.js
@@ -5,7 +5,7 @@ var dredd = new Dredd({
     server:  isTesting ? 'http://localhost:8000' : 'http://the-project.dev',
     options: {
         'server':    'php artisan serve',
-        'path':      ['docs/api/index.apib'],
+        'path':      ['resources/docs/api/index.apib'],
         'language':  isTesting ? 'vendor/bin/dredd-hooks-php' : 'dredd-hooks-php',
         'hookfiles': ['tests/dredd/hooks.php'],
         'reporter':  ['apiary'],
